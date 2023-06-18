09e9e5e7b18050b0258ba2f483bf473d74632396
Saicharan Rachamadugu <saicharan.rachamadugu@gmail.com>
2023-06-18 18:23:35 +0530

    update models.py with model 2

--- S7/models.py
+++ S7/models.py
@@ -81,3 +81,99 @@ class Model_1(nn.Module):
         x = self.convblock8(x)
         x = x.view(-1, 10)
         return F.log_softmax(x, dim=-1)
+
+class Model_04(nn.Module):
+    def __init__(self):
+        super(Model_04, self).__init__()
+        # Input Block
+        self.conv0 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=10,
+                kernel_size=(3, 3),
+                padding=0,
+                bias=False,
+            ),  # conv0_0
+            nn.ReLU(),
+        )  # output_size = 26
+
+        # Convolution 1
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=10,
+                out_channels=10,
+                kernel_size=(3, 3),
+                padding=0,
+                bias=False,
+            ),  # conv1_0
+            nn.ReLU(),  # output_size = 24
+            nn.Conv2d(
+                in_channels=10,
+                out_channels=16,
+                kernel_size=(3, 3),
+                padding=0,
+                bias=False,
+            ),  # conv1_1
+            nn.ReLU(),  # output_size = 22
+        )
+
+        # Transition 1
+        self.trans1 = nn.Sequential(
+            nn.MaxPool2d(2, 2),  # trans1_pool0 # output_size = 11
+            nn.Conv2d(
+                in_channels=16,
+                out_channels=10,
+                kernel_size=(1, 1),
+                padding=0,
+                bias=False,
+            ),  # trans1_conv0
+            nn.ReLU(),  # output_size = 11
+        )
+
+        # Convolution 2
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=10,
+                out_channels=10,
+                kernel_size=(3, 3),
+                padding=0,
+                bias=False,
+            ),  # conv2_0
+            nn.ReLU(),  # output_size = 9
+            nn.Conv2d(
+                in_channels=10,
+                out_channels=16,
+                kernel_size=(3, 3),
+                padding=0,
+                bias=False,
+            ),  # conv2_1
+            nn.ReLU(),  # output_size = 7
+        )
+
+        # Output
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=16,
+                out_channels=10,
+                kernel_size=(1, 1),
+                padding=0,
+                bias=False,
+            ),  # conv3_0
+            nn.ReLU(),  # output_size = 7
+            nn.Conv2d(
+                in_channels=10,
+                out_channels=10,
+                kernel_size=(7, 7),
+                padding=0,
+                bias=False,
+            ),  # conv3_1 # output_size = 7x7x10 | 7x7x10x10 | 1x1x10
+        )
+
+    def forward(self, x):
+        x = self.conv0(x)
+        x = self.conv1(x)
+        x = self.trans1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x, dim=-1)
